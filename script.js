b40c0fc492dc6f170d09d22826817b92ef162a34
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-04-19 20:46:08 +0200

    Add structure to display books

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+const cardContainer = document.querySelector(".card-container");
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     if(!new.target) {
         throw new Error("Constructor Book requires 'new' operator");
     }
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages) {
+    const book = new Book(title, author, pages);
     myLibrary.push(book);
-
 }
+
+// add books to myLibrary for testing purposes
+addBookToLibrary('Test1', 'test author 1', '100');
+
+function displayBooks() {
+    const uniqueId = crypto.randomUUID();
+    myLibrary.forEach(book => {
+        cardContainer.innerHTML = `
+        <div class="book-${uniqueId}">
+            <h3 class="title">${book.title}</h3>
+            <p class="author">${book.author}</p>
+            <p class="pages">${book.pages}</p>
+            <button class="read">Read</button>
+            <button class="delete">Delete</button>
+        </div>
+        `
+    });
+}
+
+displayBooks();
