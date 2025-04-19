5c9505c31abf885530adc79385f4fb911a373d73
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-04-19 16:28:43 +0200

    Add script.js with initial implementation
    - Created a new file `script.js` to start the
    project.
    - Added basic functionality to the script.

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if(!new.target) {
+        throw new Error("Constructor Book requires 'new' operator");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+
+}
